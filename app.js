1e9ca9855c16f29b61f301a9031ad2ba17c11623
Juliette Jourdan <jourdanjuliette@yahoo.fr>
2024-05-27 16:27:17 +0200

    configuration base de données mongoDB + connexion de l'api au cluster mongoDB + installation du package Mongoose

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://jourdanjuliette:5wj9PM8raElQTT0I@clustermonvieuxgrimoire.sssnb3r.mongodb.net/?retryWrites=true&w=majority&appName=ClusterMonVieuxGrimoire",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 app.use(express.json());
 
