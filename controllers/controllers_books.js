9a11a366d6fd02e0638ed4ae7344be2716dcbc68
Juliette Jourdan <jourdanjuliette@yahoo.fr>
2024-06-03 12:53:58 +0200

    modif route PUT modifyBook

--- controllers/controllers_books.js
+++ controllers/controllers_books.js
@@ -15,7 +15,7 @@ exports.createBook = (req, res, next) => {
   book
     .save()
     .then(() => {
-      res.status(200).json({ message: "Objet enregistré!" });
+      res.status(201).json({ message: "Objet enregistré!" });
     })
     .catch((error) => {
       res.status(400).json({ error });
@@ -23,11 +23,32 @@ exports.createBook = (req, res, next) => {
 };
 
 exports.modifyBook = (req, res, next) => {
-  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() =>
-      res.status(200).json({ message: "Livre modifié avec succès !" })
-    )
-    .catch((error) => res.status(404).json({ error }));
+  const bookObject = req.file
+    ? {
+        ...JSON.parse(req.body.book),
+        imageUrl: `${req.protocol}://${req.get("host")}/images/${
+          req.file.filename
+        }`,
+      }
+    : { ...req.body };
+
+  delete bookObject._userId;
+  Book.findOne({ _id: req.params.id })
+    .then((book) => {
+      if (book.userId != req.auth.userId) {
+        res.status(401).json({ message: "Non-autorisé!" });
+      } else {
+        Book.updateOne(
+          { _id: req.params.id },
+          { ...bookObject, _id: req.params.id }
+        )
+          .then(() => res.status(200).json({ message: "Objet modifié" }))
+          .catch((error) => res.status(401).json({ error }));
+      }
+    })
+    .catch((error) => {
+      res.status(400).json({ error });
+    });
 };
 
 exports.deleteBook = (req, res, next) => {
